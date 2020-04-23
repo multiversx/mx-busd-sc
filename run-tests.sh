fe59c64081f0decbf32666fb42740ad54a836a19
andrei-marinica <andrei_m_marinica@yahoo.com>
2020-04-23 19:27:07 +0300

    erc20 tests

---
+++ run-tests.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# Works with the latest version of Arwen cloned in the default location
+# Will be replaced by erdpy
+#$GOPATH/src/github.com/ElrondNetwork/arwen-wasm-vm/cmd/test/test tests
+
+$GOPATH/src/github.com/ElrondNetwork/arwen-wasm-vm/cmd/test/test tests/erc20/create.scen.json
