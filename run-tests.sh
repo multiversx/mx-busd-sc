a734e9566363f5026f190fdb4fc4f2270f677fd2
andrei-marinica <andrei_m_marinica@yahoo.com>
2020-04-24 10:26:24 +0300

    erc20 test fix and made more explicit

--- run-tests.sh
+++ run-tests.sh
@@ -3,6 +3,3 @@
 # Works with the latest version of Arwen cloned in the default location
 # Will be replaced by erdpy
 $GOPATH/src/github.com/ElrondNetwork/arwen-wasm-vm/cmd/test/test tests
-
-#$GOPATH/src/github.com/ElrondNetwork/arwen-wasm-vm/cmd/test/test tests/erc20/create.scen.json
-#$GOPATH/src/github.com/ElrondNetwork/arwen-wasm-vm/cmd/test/test tests/erc20/not_payable.scen.json
