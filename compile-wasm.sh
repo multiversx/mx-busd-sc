d1d2433eeb3ad02c9f89d566776415d2565f9f90
andrei-marinica <andrei.marinica@elrond.com>
2020-04-29 18:00:09 +0300

    fixed compilation settings

--- compile-wasm.sh
+++ compile-wasm.sh
@@ -2,9 +2,10 @@
 
 # script provided for convenience, to build and extract wasm output to root
 
-RUSTFLAGS='-C link-arg=-s' cargo build --bin busd --target=wasm32-unknown-unknown --release
-#cargo build --bin busd --target=wasm32-unknown-unknown --release
+RUSTFLAGS='-C link-arg=-s' \
+cargo build --bin busd --target=wasm32-unknown-unknown --release
 mv target/wasm32-unknown-unknown/release/busd.wasm busd.wasm
-wasm-snip busd.wasm -o busd.wasm --snip-rust-fmt-code --snip-rust-panicking-code
-#wasm-gc busd.wasm
-# twiggy top -n 100 busd.wasm > twiggy-snip.txt
+wasm-snip busd.wasm -o busd.wasm --snip-rust-fmt-code #--snip-rust-panicking-code
+# wasm-gc busd.wasm
+# twiggy top -n 20 busd.wasm
+# twiggy top -n 300 busd.wasm > twiggy-snip.txt
