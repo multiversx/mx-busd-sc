a4ac1e0fc85b885f667496654a384af40cc44236
andrei-marinica <andrei.marinica@elrond.com>
2020-04-29 19:54:21 +0300

    moved output to output folder

--- compile-wasm.sh
+++ compile-wasm.sh
@@ -4,8 +4,9 @@
 
 RUSTFLAGS='-C link-arg=-s' \
 cargo build --bin busd --target=wasm32-unknown-unknown --release
-mv target/wasm32-unknown-unknown/release/busd.wasm busd.wasm
-wasm-snip busd.wasm -o busd.wasm --snip-rust-fmt-code #--snip-rust-panicking-code
+mkdir -p output
+mv target/wasm32-unknown-unknown/release/busd.wasm output/busd.wasm
+wasm-snip output/busd.wasm -o output/busd.wasm --snip-rust-fmt-code #--snip-rust-panicking-code
 # wasm-gc busd.wasm
 # twiggy top -n 20 busd.wasm
 # twiggy top -n 300 busd.wasm > twiggy-snip.txt
